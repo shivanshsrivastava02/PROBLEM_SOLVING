1ef19309e7ccc5a3696b410521dcebfc482b36f0
SHIVANSH SRIVASTAVA <139061999+shivanshsrivastava02@users.noreply.github.com>
2023-09-26 22:51:12 +0530

    vovel.c

---
+++ vovel.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char a,e,i,o,u,w;
+printf("enter the character");
+scanf("%c",&w);
+if(w=='a'||'e'||'i'||'o'||'u'){
+printf("the number is vovel");}
+else {
+printf("the alphabate is consonate");
+}
+return 0;
+}
